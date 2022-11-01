a06d06958cc0fd384d31bcd3b11d80ce1d596e44
Ida Danielsson <idadanielsson@Idas-MBP.lan>
2022-11-01 11:14:05 +0100

    Added hamburger menu with styling

--- dist/index.aa69868b.js
+++ dist/index.aa69868b.js
@@ -1,2 +1,7 @@
+function myFunction() {
+    let x = document.getElementById("myLinks");
+    if (x.style.display === "block") x.style.display = "none";
+    else x.style.display = "block";
+}
 
 //# sourceMappingURL=index.aa69868b.js.map
